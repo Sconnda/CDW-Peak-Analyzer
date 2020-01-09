fb1540c951810d456c5d1e15eadeb0375a1935b9
Skanda Rao <svr34@scarletmail.rutgers.edu>
2020-01-09 09:18:26 -0500

    Image generation completed, peak finder started (though problems arise with recursion depth)

--- LatticePoints.py
+++ LatticePoints.py
@@ -4,7 +4,11 @@ from os import path
 import numpy as np
 
 from graphics import *
-from PIL import Image
+from PIL import Image as NewImage
+
+threshold = 0
+size_x = 512
+size_y = 512
 
 def createDataImage(filename,data):
 
@@ -15,8 +19,8 @@ def createDataImage(filename,data):
 	# Create window for drawing
 	size_x = len(data[0])
 	size_y = len(data)
-	win = GraphWin('CDW Data', size_x, size_y)
-	win.setBackground('black')
+	winGen = GraphWin('Generating CDW Image...', size_x, size_y)
+	winGen.setBackground('black')
 
 	# Draw data
 	for y in range(size_y):
@@ -25,35 +29,72 @@ def createDataImage(filename,data):
 			color = int(data[y][x]/max_point*255)
 			color = max(color,0)
 			pt.setOutline(color_rgb(color,color,color))
-			pt.draw(win)
+			pt.draw(winGen)
 
-	win.postscript(file=filename+".eps",colormode="gray")
-	win.close()
+	winGen.postscript(file=filename+".eps",colormode="gray")
+	winGen.close()
 
-	img = Image.open(filename+".eps")
+	img = NewImage.open(filename+".eps")
 	img.save(filename+".png","png")
 
 def setBackground(filename, data):
-	# True if image exists
-	existingImage = path.exists(filename+".png")
+	# True if image does not exist
+	noImage = not path.exists(filename+".png")
 
-	if existingImage:
-		size_x = len(data[0])
-		size_y = len(data)
-
-		win = GraphWin('CDW Data', size_x, size_y)
-		win.setBackground('black')
-
-		img = Image(Point(0,0), filename+".png")
-		print("Imagine all the people")
-	else:
+	if noImage:
 		createDataImage(filename, data)
+
+	img = Image(Point(size_x/2,size_y/2), filename+".png")
+	return img
+
+def findNeighbors(data,win,x,y):
+	peak = [[data[y][x],x,y]]
+	data[y][x] = 0
+	pt = Point(x,y)
+	pt.setOutline(color_rgb(255,0,0))
+	pt.draw(win)
+
+	for dy in [-1,0,1]:
+		for dx in [-1,0,1]:
+			if data[y+dy][x+dx] > threshold:
+				peak2,data = findNeighbors(data,win,x+dx,y+dy)
+				peak += peak2
+				print(data[y][x])
+				time.sleep(0.1)
+
+	return peak,data
+
+def findPeaks(data, win):
+	data2 = data
+	peaks = []
+
+	# !!Change back to size_x, size_y
+	for y in range(50):
+		for x in range(50):
+			if data2[y][x] > threshold:
+				peak,data2 = findNeighbors(data2, win, x,y)
+				peaks.append(peak)
+
+	print(peaks)
+	return 0
 
 def main():
 	filename = "CDW_GonTaS2"
 	data = np.loadtxt(filename+".txt")
 
+	size_x = len(data[0])
+	size_y = len(data)
+
+	win = GraphWin('CDW Data', size_x, size_y)
+	win.setBackground('black')
+
 	# Show CDW images
-	setBackground(filename, data)
+	bg = setBackground(filename, data)
+	bg.draw(win)
+
+	# findPeaks(data, win)
+
+	win.getMouse()
+	win.close()
 
 main()
